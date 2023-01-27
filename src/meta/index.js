7d57a4e196219246b1dca35492c2c3a3cc06fe24
vasteymnathan <nmvastey@gmail.com>
2023-01-26 22:14:02 -0500

    Same as last commit, forgot to include index.js in git add

--- src/meta/index.js
+++ src/meta/index.js
@@ -1,73 +1,67 @@
-'use strict';
-
-const winston = require('winston');
-const os = require('os');
-const nconf = require('nconf');
-
-const pubsub = require('../pubsub');
-const slugify = require('../slugify');
-
-const Meta = module.exports;
-
-Meta.reloadRequired = false;
-
-Meta.configs = require('./configs');
-Meta.themes = require('./themes');
-Meta.js = require('./js');
-Meta.css = require('./css');
-Meta.settings = require('./settings');
-Meta.logs = require('./logs');
-Meta.errors = require('./errors');
-Meta.tags = require('./tags');
-Meta.dependencies = require('./dependencies');
-Meta.templates = require('./templates');
-Meta.blacklist = require('./blacklist');
-Meta.languages = require('./languages');
-
-
-/* Assorted */
-Meta.userOrGroupExists = async function (slug) {
-    if (!slug) {
-        throw new Error('[[error:invalid-data]]');
-    }
-    const user = require('../user');
-    const groups = require('../groups');
-    slug = slugify(slug);
-    const [userExists, groupExists] = await Promise.all([
-        user.existsBySlug(slug),
-        groups.existsBySlug(slug),
-    ]);
-    return userExists || groupExists;
-};
-
-if (nconf.get('isPrimary')) {
-    pubsub.on('meta:restart', (data) => {
-        if (data.hostname !== os.hostname()) {
-            restart();
-        }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-}
-
-Meta.restart = function () {
-    pubsub.publish('meta:restart', { hostname: os.hostname() });
-    restart();
 };
-
-function restart() {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSessionTTLSeconds = exports.restart = exports.userOrGroupExists = void 0;
+const winston_1 = __importDefault(require("winston"));
+const os_1 = __importDefault(require("os"));
+const nconf_1 = __importDefault(require("nconf"));
+const pubsub_1 = __importDefault(require("../pubsub"));
+const slugify_1 = __importDefault(require("../slugify"));
+const configs_1 = __importDefault(require("./configs"));
+const user_1 = __importDefault(require("../user"));
+const groups_1 = __importDefault(require("../groups"));
+/* Assorted */
+function restart_helper() {
     if (process.send) {
         process.send({
             action: 'restart',
         });
-    } else {
-        winston.error('[meta.restart] Could not restart, are you sure NodeBB was started with `./nodebb start`?');
+    }
+    else {
+        winston_1.default.error('[meta.restart] Could not restart, are you sure NodeBB was started with `./nodebb start`?');
     }
 }
-
-Meta.getSessionTTLSeconds = function () {
-    const ttlDays = 60 * 60 * 24 * Meta.config.loginDays;
-    const ttlSeconds = Meta.config.loginSeconds;
+function userOrGroupExists(slug) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!slug) {
+            throw new Error('[[error:invalid-data]]');
+        }
+        const slugified = (0, slugify_1.default)(slug);
+        const [userExists, groupExists] = yield Promise.all([
+            user_1.default.existsBySlug(slugified),
+            groups_1.default.existsBySlug(slugified),
+        ]);
+        return userExists || groupExists;
+    });
+}
+exports.userOrGroupExists = userOrGroupExists;
+if (nconf_1.default.get('isPrimary')) {
+    pubsub_1.default.on('meta:restart', (data) => {
+        if (data.hostname !== os_1.default.hostname()) {
+            restart_helper();
+        }
+    });
+}
+function restart() {
+    pubsub_1.default.publish('meta:restart', { hostname: os_1.default.hostname() });
+    restart_helper();
+}
+exports.restart = restart;
+function getSessionTTLSeconds() {
+    const ttlDays = 60 * 60 * 24 * configs_1.default.loginDays;
+    const ttlSeconds = configs_1.default.loginSeconds;
     const ttl = ttlSeconds || ttlDays || 1209600; // Default to 14 days
     return ttl;
-};
-
-require('../promisify')(Meta);
+}
+exports.getSessionTTLSeconds = getSessionTTLSeconds;
